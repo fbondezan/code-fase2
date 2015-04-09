39a644c5b82b6531a46dd1d27ae2a78f07eba3dc
Felipe Bondezan <vegar@vegar.com.br>
2015-04-09 01:19:35 +0000

    Primeiro commit

---
+++ menu.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="?pag=home">HOME</a></li>
+        <li><a href="?pag=empresa">EMPRESA</a></li>
+        <li><a href="?pag=produtos">PRODUTOS</a></li>
+        <li><a href="?pag=servicos">SERVIÇOS</a></li>
+        <li><a href="?pag=contato">CONTATO</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+<!--
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+<ul class="nav navbar-nav"> 
+	<li> HOME </li>
+	<li> EMPRESA </li>
+	<li> PRODUTOS </li>
+	<li> SERVIÇOS </li>
+	<li> CONTATO </li>	
+</ul>
+</div>
+-->
